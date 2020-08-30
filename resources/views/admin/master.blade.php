43071e1babc5efe83f86d78c838b35b8522d09a6
Md. Abdullah <68258605+Ahsum1558@users.noreply.github.com>
2020-08-30 09:45:10 +0600

    Install and Admin Login System Setup

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,8 @@
+@include('admin.includes.header')
+
+@yield('main-content')
+{{-- @yield('sometext', View::make('admin.home.home')) --}}
+{{-- @include('admin.home.slider') --}}
+
+
+@include('admin.includes.footer')
